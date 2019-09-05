33a14d84f917b72724abac18150478431bad3a86
codegymdanang <codegymdanang@gmail.com>
2019-09-05 16:45:46 +0700

    inheritance

---
+++ src/main/java/codegym/danang/abstraction/Main.java
@@ -0,0 +1,4 @@
+package codegym.danang.abstraction;
+
+public class Main {
+}
---
+++ src/main/java/codegym/danang/abstraction/Television.java
@@ -0,0 +1,12 @@
+package codegym.danang.abstraction;
+
+public class Television {
+
+    public void openTelevision() {
+
+    }
+
+    public void openChannel() {
+
+    }
+}
